81550cbb17108099592594cfa5448047e1dc734b
Brad Duck <brad.w.duck@gmail.com>
2021-02-05 11:13:23 -0500

    Updates

---
+++ precache-manifest.07d69f0bfc5054bc59272bc3905e44e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da306940cc9496a8e26aab67d23f6cc9",
+    "url": "/kinaxis-hangman/index.html"
+  },
+  {
+    "revision": "c202cccea6d1e4ad2886",
+    "url": "/kinaxis-hangman/static/css/main.2d61d5da.chunk.css"
+  },
+  {
+    "revision": "b27d602b5bce1f337178",
+    "url": "/kinaxis-hangman/static/js/2.d238f3b5.chunk.js"
+  },
+  {
+    "revision": "c202cccea6d1e4ad2886",
+    "url": "/kinaxis-hangman/static/js/main.9d68a33c.chunk.js"
+  },
+  {
+    "revision": "395c603ff18ef67c385b",
+    "url": "/kinaxis-hangman/static/js/runtime~main.75d88a47.js"
+  }
+]);
